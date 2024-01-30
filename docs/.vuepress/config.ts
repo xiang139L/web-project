9d59e9b1fbc9089957dedfb9e5f97ff2f1c7e9eb
xiang <166152086@qq.com>
2024-01-30 15:06:08 +0800

    '1'

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,22 @@
+import { defineConfig4CustomTheme } from 'vuepress/config'
+
+export default defineConfig4CustomTheme({
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: '磊子的博客 ',
+      description: '个人笔记',
+    },
+  },
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '前端', link: '/web/' },
+      { text: '后端', link: '/back/' },
+      { text: '算法', link: '/algorithm/' },
+      { text: '工具', link: '/tools/' },
+      { text: '关于', link: '/about/' },
+      { text: 'Github', link: '' },
+    ],
+  },
+})
